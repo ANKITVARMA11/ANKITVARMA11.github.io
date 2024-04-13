15d7cb70c542df966a550a9030e6315515910721
ANKITVARMA11 <ankitvarma3708@gmail.com>
2024-04-13 20:05:01 +0530

    UPDATE

--- Script.js
+++ Script.js
@@ -1,2 +1,4 @@
-let abc = "Hello World!";
-document.write(abc)
+// GETTING OUT CANVAS ELEMENT AND SETTING THE CONTEXT
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d")
+
