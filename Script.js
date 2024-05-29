815764cf395af4e4229cb662e7d1b56e2882ecdb
ANKITVARMA11 <ankitvarma3708@gmail.com>
2024-05-29 15:18:33 +0530

    Update

--- Script.js
+++ Script.js
@@ -1,4 +1,28 @@
 // GETTING OUT CANVAS ELEMENT AND SETTING THE CONTEXT
-const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d")
+// let a = 5;
+// let b = 3;
+// console.log(":a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+// console.log("a**b",a**b);
+// console.log("a++ =",a++);
+// console.log("a-- = ",a--);
+// console.log("++a =",++a);
+// console.log("--a = ",--a);
+// let cond1  = a>b;
+// let cond2 = b<a;
+// let cond3 = b>a;
+// console.log("cond1&&cond2",cond1&&cond2); //true (logical AND)
+// console.log("cond2||cond3",cond2||cond3); //true (logical OR)
+// console.log("not a cond1",!(cond1)); //cond1 is true but it will make it false
 
+// if(a>0){
+//     console.log("Yeah! variable 'a' is greater than 0")
+// }
+
+let a ="Adharmi"
+if(a){
+    alert("You are an adharmi!!")
+}
